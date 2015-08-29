8d1265e12bec32923bf441a9918b8dae18b2c657
Julien Cumin <juliencumin@yahoo.fr>
2015-08-29 18:51:54 +0200

    Added support for atoms and numbers for predicates Behead and Reverse

--- src/brachylog/BrachylogPredicates.java
+++ src/brachylog/BrachylogPredicates.java
@@ -11,18 +11,33 @@ public final class BrachylogPredicates {
 				+ "    reverse(C,D),\n"
 				+ "    string_codes(Y,D)\n"
 				+ "    ;\n"
-				+ "    reverse(X,Y).\n"
-		
-				+ "\n"
-				+ "reverse(Xs, Ys) :-\n"
-				+ "    reverse(Xs, [], Ys, Ys).\n"
-				+ "reverse([], Ys, Ys, []).\n"
-				+ "reverse([X|Xs], Rs, Ys, [_|Bound]) :-\n"
-				+ "    reverse(Xs, [X|Rs], Ys, Bound).\n";
-		
+				+ "    number(X),!,\n"
+				+ "    number_codes(X,E),\n"
+				+ "    reverse(E,F),\n"
+				+ "    number_codes(Y,F)\n"
+				+ "    ;\n"
+				+ "    atom(X),!,\n"
+				+ "    atom_codes(X,G),\n"
+				+ "    reverse(G,H),\n"
+				+ "    atom_codes(Y,H)\n"
+				+ "    ;\n"
+				+ "    reverse(X,Y).\n";
 		return s;
 	}
 
+	
+	public static String pLength() {
+		
+		String s = "\n"
+				+ Constants.P_LENGTH + "(X,Y) :-\n"
+				+ "    (atom(X) ; number(X) ; string(X)),!,\n"
+				+ "    atom_length(X,Y)\n"
+				+ "    ;\n"
+				+ "    length(X,Y).\n";
+		
+		return s;
+	}
+	
 	
 	public static String pBehead() {
 		
@@ -30,6 +45,14 @@ public final class BrachylogPredicates {
 				+ Constants.P_BEHEAD + "(X,Y) :-\n"
 				+ "    string(X),!,\n"
 				+ "    sub_string(X, 1, _, 0, Y)\n"
+				+ "    ;\n"
+				+ "    number(X),!,\n"
+				+ "    number_codes(X,[_|T]),\n"
+				+ "    number_codes(Y,T)\n"
+				+ "    ;\n"
+				+ "    atom(X),!,\n"
+				+ "    atom_codes(X,[_|U]),\n"
+				+ "    atom_codes(Y,U)\n"
 				+ "    ;\n"
 				+ "    X = [_|Y].\n";
 		
