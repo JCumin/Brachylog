cfdd01dbd49135ecc30861d734980c33dd337071
Julien Cumin <juliencumin@yahoo.fr>
2016-06-05 12:46:58 +0200

    Implemented 2 to 9 n-chotomy

--- prolog_parser/string_predicates.pl
+++ prolog_parser/string_predicates.pl
@@ -1,5 +1,13 @@
 :- module(string_predicates, [brachylog_string_lowercase/2,
-                              brachylog_string_uppercase/2]).
+                              brachylog_string_uppercase/2,
+                              brachylog_string_dichotomize/2,
+                              brachylog_string_trichotomize/2,
+                              brachylog_string_tetrachotomize/2,
+                              brachylog_string_pentachotomize/2,
+                              brachylog_string_hexachotomize/2,
+                              brachylog_string_heptachotomize/2,
+                              brachylog_string_octachotomize/2,
+                              brachylog_string_enneachotomize/2]).
                        
 :- use_module(library(clpfd)).
 :- use_module(utils).
@@ -15,3 +23,49 @@ BRACHYLOG_STRING_UPPERCASE
 */
 brachylog_string_uppercase('string':Ls0,'string':Ls) :-
     maplist(upcase_atom, Ls0, Ls).
+
+/*
+BRACHYLOG_STRING_NCHOTOMIZE
+*/
+brachylog_string_dichotomize(X,[A,B]) :-
+    brachylog_string_Nchotomize(X,[A,B]).
+
+brachylog_string_trichotomize(X,[A,B,C]) :-
+    brachylog_string_Nchotomize(X,[A,B,C]).
+
+brachylog_string_tetrachotomize(X,[A,B,C,D]) :-
+    brachylog_string_Nchotomize(X,[A,B,C,D]).
+
+brachylog_string_pentachotomize(X,[A,B,C,D,E]) :-
+    brachylog_string_Nchotomize(X,[A,B,C,D,E]).
+
+brachylog_string_hexachotomize(X,[A,B,C,D,E,F]) :-
+    brachylog_string_Nchotomize(X,[A,B,C,D,E,F]).
+
+brachylog_string_heptachotomize(X,[A,B,C,D,E,F,G]) :-
+    brachylog_string_Nchotomize(X,[A,B,C,D,E,F,G]).
+
+brachylog_string_octachotomize(X,[A,B,C,D,E,F,G,H]) :-
+    brachylog_string_Nchotomize(X,[A,B,C,D,E,F,G,H]).
+
+brachylog_string_enneachotomize(X,[A,B,C,D,E,F,G,H,I]) :-
+    brachylog_string_Nchotomize(X,[A,B,C,D,E,F,G,H,I]).
+    
+brachylog_string_Nchotomize('string':L,L2) :-
+    (
+        var(L)
+        -> maplist(prepend_string,M,L2),
+        brachylog_string_Nchotomize(L,M)
+        ;
+        length(L2,Length),
+        length(M,Length),
+        brachylog_string_Nchotomize(L,M),
+        maplist(prepend_string,M,L2)
+    ). 
+brachylog_string_Nchotomize(L,L2) :-
+    append(L2,L),
+    length(L,LengthL),
+    length(L2,LengthL2),
+    Length #= (LengthL + 1)//LengthL2,
+    reverse(L2,[_|T]),
+    maplist(length_(Length),T).
