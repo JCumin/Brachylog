80060dfa7636436a894eb713f6cb04ad1b1ee74d
Julien Cumin <juliencumin@yahoo.fr>
2016-08-12 08:59:30 +0200

    Implemented all_equal

--- prolog_parser/constraint_predicates.pl
+++ prolog_parser/constraint_predicates.pl
@@ -15,6 +15,7 @@ ____            ____
 
 
 :- module(constraint_predicates, [brachylog_constraint_different/2,
+                                  brachylog_constraint_all_equal/2,
                                   brachylog_constraint_coerce_to_integer/2,
                                   brachylog_constraint_coerce_to_string/2,
                                   brachylog_constraint_coerce_to_list/2
@@ -25,10 +26,10 @@ ____            ____
  
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   BRACHYLOG_CONSTRAINT_COERCE_TO_INTEGER
+   BRACHYLOG_CONSTRAINT_DIFFERENT
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
- brachylog_constraint_different([],[]).
- brachylog_constraint_different([H|T],[H|T]) :-
+brachylog_constraint_different([],[]).
+brachylog_constraint_different([H|T],[H|T]) :-
     maplist(prepend_integer,L,[H|T]),
     all_distinct(L).
 brachylog_constraint_different('integer':0,'integer':0).
@@ -36,6 +37,21 @@ brachylog_constraint_different('integer':I,'integer':I) :-
     H #\= 0,
     integer_value('integer':_:[H|T],I),
     all_distinct([H|T]).
+    
+    
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   BRACHYLOG_CONSTRAINT_ALL_EQUAL
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+brachylog_constraint_all_equal([],[]).
+brachylog_constraint_all_equal([H|T],[H|T]) :-
+    maplist(=(H),T).
+brachylog_constraint_all_equal('string':L,'string':L) :-
+    brachylog_constraint_all_equal(L,L).
+brachylog_constraint_all_equal('integer':0,'integer':0).
+brachylog_constraint_all_equal('integer':I,'integer':I) :-
+    H #\= 0,
+    integer_value('integer':_:[H|T],I),
+    brachylog_constraint_all_equal([H|T],[H|T]).
  
  
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -47,7 +63,7 @@ brachylog_constraint_coerce_to_integer('integer':I,'integer':I).
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    BRACHYLOG_CONSTRAINT_COERCE_TO_STRING
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-brachylog_constraint_coerce_to_string('string':I,'string':I).
+brachylog_constraint_coerce_to_string('string':S,'string':S).
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
